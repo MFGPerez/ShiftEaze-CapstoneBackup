d84998f923060ad2a86b24656ea1962c341b1acf
Marcel Gallardo <116410849+MFGPerez@users.noreply.github.com>
2024-08-06 16:09:14 -0600

    update

--- app/components/faqsContactManagerNavBar.js
+++ app/components/faqsContactManagerNavBar.js
@@ -1,7 +1,6 @@
 "use client";
-
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 /**
  * SupportNavBar Component
@@ -12,17 +11,28 @@ import Link from 'next/link';
  * @returns {JSX.Element} The SupportNavBar component
  */
 const SupportNavBar = () => {
+  const router = useRouter();
+
+  /**
+   * Handles the back button click event.
+   * 
+   * Navigates the user to the previous page.
+   */
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <nav className="bg-black opacity-85 text-white py-3 w-full">
       <div className="flex justify-between items-center px-4">
-        <div className="text-xl font-rockSalt">ShiftEaze</div>
+        <div className="text-xl font-rockSalt">ShiftEaze!</div>
         <div className="flex space-x-6 items-center">
-          <Link
-            href="/punchinOut"
+        <button
+            onClick={handleBack}
             className="bg-gray-500 text-white px-4 py-2 rounded-md inline-block shadow-md hover:bg-gray-600 border-2 border-transparent hover:border-gray-400 font-comfortaa font-semibold"
           >
-            Back to Worker Search
-          </Link>
+            Back to Punch in
+          </button>
         </div>
       </div>
     </nav>
@@ -30,3 +40,9 @@ const SupportNavBar = () => {
 };
 
 export default SupportNavBar;
+
+
+
+
+
+
